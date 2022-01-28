9e4d81d44bd42ff96d93f3e94016729c23f2a0f0
jenobpj <jenobgit@gmail.com>
2022-01-27 19:11:34 -0500

    add router and error handlers

--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./Data/products.js";
 import connectDB from "./config/db.js";
-const app = express();
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
+
 connectDB();
 
+const app = express();
 app.get("/", (req, res) => {
   res.send("API IS RUNNING");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5050;
 
