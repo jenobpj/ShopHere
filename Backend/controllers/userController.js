96d808d6faedbafb4126d2415b1039d63c82f4be
jenobpj <jenobgit@gmail.com>
2022-02-03 15:17:11 -0500

    add controller for getting user and creating token

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,9 +1,9 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import generateToken from "../Utils/generateToken.js";
 
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-  res.send({ email, password });
   const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
     res.json({
@@ -11,7 +11,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
@@ -19,4 +19,19 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("user not found");
+  }
+});
+
+export { authUser, getUserProfile };
