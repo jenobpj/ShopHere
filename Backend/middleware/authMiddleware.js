9f7f34dfd917921f4a7db68ae19e396a539fa4b8
jenobpj <jenobgit@gmail.com>
2022-02-07 13:15:47 -0500

    fix the bug in next()

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -12,6 +12,7 @@ const protect = asyncHandler(async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select("-password");
+      next();
     } catch (error) {
       console.error(error);
       res.status(401);
@@ -22,6 +23,5 @@ const protect = asyncHandler(async (req, res, next) => {
     res.status(401);
     throw new Error("Not autorized,no token");
   }
-  next();
 });
 export { protect };
