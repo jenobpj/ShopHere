aa7fd3f4928823e9125aaaa060c7b239a09db4bd
jenobpj <jenobgit@gmail.com>
2022-02-07 10:50:36 -0500

    add action for getting userDetails

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import {
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
@@ -8,11 +10,11 @@ import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
 } from "../constants/userConstants";
-
+//login action middleware
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({
-      type: USER_LOGIN_REQUEST,
+      type: USER_LOGIN_REQUEST, //login process requested
     });
 
     const config = {
@@ -26,12 +28,12 @@ export const login = (email, password) => async (dispatch) => {
       { email, password },
       config
     );
-    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data }); //login sucees pass the data as payload
 
-    localStorage.setItem("userInfo", JSON.stringify(data));
+    localStorage.setItem("userInfo", JSON.stringify(data)); //set userInfo state in localstorage
   } catch (error) {
     dispatch({
-      type: USER_LOGIN_FAIL,
+      type: USER_LOGIN_FAIL, //if it is fail
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
@@ -62,13 +64,43 @@ export const register = (name, email, password) => async (dispatch) => {
       { name, email, password },
       config
     );
-    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
-    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data }); //it saves the data registration state
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data }); //it saves the data userLogin redux store it used to check user authentication in fronend side
 
     localStorage.setItem("userInfo", JSON.stringify(data));
   } catch (error) {
     dispatch({
       type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+//For user details
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  //user details send with token and it will reach protected route and it finds usere id and reterive user information
+  try {
+    dispatch({
+      type: USER_DETAILS_REQUEST,
+    });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/users/${id}`, config); //this request send with token and token will verify in protect middlewate at backend
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
