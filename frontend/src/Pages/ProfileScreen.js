346943f62aa6bae07d3bdfe3a8b5bfe9c18be1e3
jenobpj <jenobgit@gmail.com>
2022-02-08 01:07:34 -0500

    add profileScreen

--- frontend/src/Pages/ProfileScreen.js
+++ frontend/src/Pages/ProfileScreen.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Link, useSearchParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../Components/Message";
 import Loader from "../Components/Loader.js";
-import { getUserDetails } from "../actions/userAction.js";
+import { getUserDetails, updateUserProfile } from "../actions/userAction.js";
+import { USER_UPDATE_PROFILE_RESET } from "../constants/userConstants";
 
 const ProfileScreen = () => {
   const [name, setName] = useState("");
@@ -16,25 +17,29 @@ const ProfileScreen = () => {
 
   const dispatch = useDispatch();
 
-  const userDetails = useSelector((state) => state.userDetails);
-  const { loading, error, user } = userDetails; //when it first render  userDetails does not have value
-
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin; //check userLogin or not
+
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, error, user } = userDetails; //when it first render  userDetails does not have value;
+
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const { success } = userUpdateProfile;
 
   useEffect(() => {
     if (!userInfo) {
       navigate("/login");
     } else {
-      if (!user.name) {
+      if (!user || !user.name || success) {
         //if there is no name it will request to  get the userDetails,it will store in userDetails
+        dispatch({ type: USER_UPDATE_PROFILE_RESET });
         dispatch(getUserDetails("profile"));
       } else {
         setName(user.name);
         setEmail(user.email);
       }
     }
-  }, [dispatch, userInfo]);
+  }, [dispatch, userInfo, user, success]);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -42,8 +47,7 @@ const ProfileScreen = () => {
     if (password !== confirmPassword) {
       setMessage("Passwords do not match");
     } else {
-      setMessage(null);
-      dispatch(register(name, email, password));
+      dispatch(updateUserProfile({ id: user._id, name, email, password }));
     }
   };
 
@@ -53,12 +57,13 @@ const ProfileScreen = () => {
         <h2>User Profile</h2>
         {message && <Message variant="danger">{message}</Message>}
         {error && <Message variant="danger">{error}</Message>}
+        {success && <Message variant="success">Profile Updated</Message>}
         {loading && <Loader />}
         <Form onSubmit={submitHandler}>
           <Form.Group controlId="name">
             <Form.Label>Name</Form.Label>
             <Form.Control
-              type="name"
+              type="text"
               placeholder="Enter name"
               value={name}
               onChange={(e) => setName(e.target.value)}
@@ -85,7 +90,7 @@ const ProfileScreen = () => {
             ></Form.Control>
           </Form.Group>
 
-          <Form.Group controlId="confirmpassword">
+          <Form.Group controlId="confirmPassword">
             <Form.Label>Confirm password</Form.Label>
             <Form.Control
               type="password"
